51c87577a017aa2e9fb74d879e437cdd44dd8d49
ssstttaaasssiiikk@gmail.com <ssstttaaasssiiikk@gmail.com>
2023-04-20 01:40:39 +0700

    modified:   .metadata 	modified:   android/build.gradle 	new file:   lib/UI/colors.dart 	modified:   lib/main.dart 	new file:   lib/notifications/notifications_page.dart 	new file:   lib/notifications/pattern/notification_pattern.dart 	modified:   pubspec.lock 	new file:   pubspec.yaml 	new file:   windows/.gitignore#	new file:   windows/CMakeLists.txt 	new file:   windows/flutter/CMakeLists.txt 	new file:   windows/flutter/generated_plugin_registrant.cc 	new file:   windows/flutter/generated_plugin_registrant.h 	new file:   windows/flutter/generated_plugins.cmake 	new file:   windows/runner/CMakeLists.txt 	new file:   windows/runner/Runner.rc 	new file:   windows/runner/flutter_window.cpp 	new file:   windows/runner/flutter_window.h 	new file:   windows/runner/main.cpp 	new file:   windows/runner/resource.h 	new file:   windows/runner/resources/app_icon.ico 	new file:   windows/runner/runner.exe.manifest 	new file:   windows/runner/utils.cpp 	new file:   windows/runner/utils.h 	new file:   windows/runner/win32_window.cpp 	new file:   windows/runner/win32_window.h

---
+++ lib/UI/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color black = Color.fromRGBO(26, 18, 0, 1);
+  static const Color lightYellow = Color.fromRGBO(255, 241, 204, 1);
+  static const Color darkYellow = Color.fromRGBO(255, 212, 102, 1);
+  static const Color brown = Color.fromRGBO(51, 37, 0, 1);
+}
---
+++ lib/notifications/notifications_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:vkyrse/notifications/pattern/notification_pattern.dart';
+
+class Notifications extends StatelessWidget {
+  const Notifications({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          backgroundColor:
+              const Color.fromRGBO(255, 197, 51, 1), //вынести в отдельный класс
+          title: const Center(child: Text('Уведомления'))),
+      body: const MyScrean(),
+    ));
+  }
+}
+
+class MyScrean extends StatelessWidget {
+  const MyScrean({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(255, 248, 229, 1),
+        body: Center(
+            child: Column(
+          children: [
+            const NotificationPattern('какая-то задача'),
+            const NotificationPattern('какая-то задача')
+          ],
+        )));
+  }
+}
