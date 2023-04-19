a422977202b352208eabe28385d5bc8a0dc34145
justGyga <126105889+justGyga@users.noreply.github.com>
2023-04-19 11:24:47 +0600

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFFB800);
+const kMainTextColor = Color(0xFF332500);
+const kBackGroundColor = Color(0xFFF0E6E0);
+const kFrameTextColor = Color(0xFFFFD466);
+const kFrameBackgroundColor = Color(0xFF332500);
+
+const double kDefaultPadding = 10.0;
