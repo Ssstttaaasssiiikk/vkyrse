d333b72d89b3fad1d237c0928835e4d309e908c7
justGyga <iwantdrink229@gmail.com>
2023-04-20 22:24:53 +0700

    TimeTable

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'news/news_page.dart';
+import 'timetable/time_table.dart';
+// import 'news/news_page.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/', //нужен для организации навигации в приложении
-  routes: {'/': (context) => const NewsPage()}));
+  routes: {'/': (context) => const TimeTablePage()}));
