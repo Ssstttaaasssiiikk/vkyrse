4d4c5f7b7ab6de38d78c9c20a4d0bf7b39eb38a5
justGyga <iwantdrink229@gmail.com>
2023-04-20 17:43:23 +0700

    Added News Page, constants.dart, AppBar in patterns

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,5 +5,6 @@ const kMainTextColor = Color(0xFF332500);
 const kBackGroundColor = Color(0xFFF0E6E0);
 const kFrameTextColor = Color(0xFFFFD466);
 const kFrameBackgroundColor = Color(0xFF332500);
+const kFrameBackgroundColor2 = Color(0xFF332500);
 
 const double kDefaultPadding = 10.0;
