00bce7d845f8807140c6e9f06c03a23055e0c537
kEka-KuN <rodgney226@gmail.com>
2023-04-20 21:25:39 +0700

    добавление задачи и её имени

--- lib/ToDoList.dart
+++ lib/ToDoList.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 
 import 'Tusk.dart';
 
+class Tusk {
+  Widget Mew(String nameTusk) {
+    return Container(
+        padding: EdgeInsets.all(5),
+        alignment: Alignment.centerLeft,
+        width: double.infinity,
+        height: 70,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 234, 245, 79),
+            borderRadius: BorderRadius.circular(10)),
+        child: Text(nameTusk,
+            style: TextStyle(fontSize: 26, backgroundColor: Colors.orange)));
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -48,7 +64,32 @@ class _MainPage extends State<MainPage> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: creatusk,
+        onPressed: () {
+          String nameTusk = '';
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('имя задачи'),
+                content: TextField(
+                  onChanged: (String value) {
+                    nameTusk = value;
+                  },
+                ),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          todo.add(Tusk().Mew(nameTusk));
+                        });
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('добавить'))
+                ],
+              );
+            },
+          );
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
